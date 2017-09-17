0284708718f8f8ebcb6329f9dd081a73cef2d2aa
NathanDeMaria <nathan.demaria@hudl.com>
2017-09-16 23:37:20 -0500

    More general data format

--- R/data.R
+++ R/data.R
@@ -1,7 +1,18 @@
 library(dplyr)
-library(lubridate)
 library(magrittr)
 library(readr)
+library(purrr)
+
+
+.check_duplicates <- function(day_matches) {
+  players <- c(day_matches$winner, day_matches$loser)
+  dupes <- duplicated(players)
+  if (any(dupes)) {
+    stop(sprintf("Found players with multiple games on %s: %s",
+                 day_matches$date[1],
+                 paste(players[dupes], collapse = ', ')))
+  }
+}
 
 
 #' Load CSV
@@ -11,12 +22,25 @@ library(readr)
 #' @return
 #' @export
 load_results <- function(csv_path) {
-  results <- readr::read_csv(csv_path)
+  col_types <- cols(
+    date = col_date(),
+    group = col_character(),
+    season = col_integer(),
+    winner = col_character(),
+    loser = col_character(),
+    winner_score = col_number(),
+    loser_score = col_number()
+  )
+  results <- read_csv(csv_path, col_types = col_types)
 
-  required_columns <- c('week', 'league_group', 'season', 'winner', 'loser', 'winner_sets', 'loser_sets')
+  required_columns <- names(col_types$cols)
   missed_requirements <- !required_columns %in% colnames(results)
-  if(any(missed_requirements)) {
-    stop("Missing required columns: ", required_columns[missed_requirements])
+  if (any(missed_requirements)) {
+    stop("Missing required columns: ",
+         paste(required_columns[missed_requirements], collapse = ', '))
   }
-  results %>% dplyr::mutate(week = lubridate::ymd(week)) %>% filter(!is.na(winner))
+
+  # Each player can only play one game per date
+  results %>% group_by(date) %>% map(~.check_duplicates)
+  results
 }
